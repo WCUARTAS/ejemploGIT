7bd383c018377af361ae53b80561d388e691e99a
WILLIAN <WILLIAN@DESKTOP-CDKLCO1>
2021-03-12 07:42:56 -0500

    agregar boton a formulario 1

--- ejemploGIT/Form1.cs
+++ ejemploGIT/Form1.cs
@@ -16,5 +16,11 @@ namespace ejemploGIT
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 formulario = new Form2();
+            formulario.Show();
+        }
     }
 }
