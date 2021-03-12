5f9f7e01cfd5cd4bd488a18bbae3626b9f1cb3be
WILLIAN <WILLIAN@DESKTOP-CDKLCO1>
2021-03-12 07:59:14 -0500

    creacion de la clase persona con variables

---
+++ ejemploGIT/persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploGIT
+{
+    class persona
+    {
+        String cedula,nombre;
+        int ano_nacimiento, edad;
+
+    }
+}
